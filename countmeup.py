c26abec45ee18b10255d3328f7b73cd6cc519993
Ugur Bastug <bastugur@gmail.com>
2017-03-12 16:49:35 +0000

    user log in/register exception handling. vote casting protected by server. code comments, various backend communications.

--- countmeup.py
+++ countmeup.py
@@ -3,11 +3,13 @@ import pdb as debugger;
 import time
 import random
 import pickle
-import multiprocessing
 
+import multiprocessing
 from multiprocessing import Manager
 from multiprocessing import Process
+#Parallel libraries
 
+#user class mainly for encapsulating the voteCast variable
 class User:
    def __init__(self, name):
       self.name = name
@@ -43,6 +45,7 @@ def requestCount():
 
 
 def requestPercentage(results):
+	#serial implementation. That was the first code I wrote. Not currently in use.
 	start = time.time()
 	for candidate in results:
 		print candidate, 100*results[candidate]/sum(results.values())
@@ -51,6 +54,8 @@ def requestPercentage(results):
 		print("1 sec threshold satisfied")
 
 def canVote(user):
+	#client side check. Not really important since the first/main check happens in the server side.
+	#server.py line: 63
 	if user.votesCast<3:
 		user.votesCast+=1
 		return True;
@@ -71,42 +76,54 @@ def randomword(length):
    return ''.join(random.choice(string.lowercase) for i in range(length))
 
 def simulateLargeVotes():
+	#This wont be executed. I ran this one in order to simulate the votes earlier and saved it in a file
 	for x in xrange(0,10000000):
 		randomCandidate = "candidate-"+str(random.randint(1, 5));
 		vote( User("someone"),randomCandidate);
+		#User("someone") because it does not really matter who voted what, I still keep the username field in order to make the code flexible for future requirements.
 	with open('votes.pickle', 'wb') as handle:
 	    pickle.dump(results, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 def calculateCandidate(pid,shared):
 	target = ('candidate-'+str(pid+1))
+	#pid+1 instead of pid in order to avoid confusion such as thread 0 = candidate 1
 	shared[pid+1]=results[target]
 
 def requestPercentageMultiProcess():
 	start = time.time()
 
    	jobs = []
-
+   	#all processes will be kept in an array
    	manager = multiprocessing.Manager()
    	shared = manager.dict()
+   	#global variable
 
    	for i in range(5):
+   		#I decided to create 5 child processes that run simultanously. Each process is responsible for one candidate.
 		p = multiprocessing.Process(target=calculateCandidate, args=(i,shared))
+		#Each process also utilises the global variable shared in order to save the count result
 		jobs.append(p)
+		#Save the child process to the array
 		p.start()
 	for j in jobs:
+		#Join the processes when the job is finished
 		j.join()
+	#Since we joined all the processes that are done. We now have only a single process going on.
+	#The main thread is resposible for recording one single variable the sum of all votes.
 	shared["Total Votes"]=sum(results.values())
+	#Print the recorded time spent in the execution
 	print "execution time", time.time() - start, "s."
 	return shared.copy()
 
 
-
+#Init data
 results["candidate-1"]=0
 results["candidate-2"]=0
 results["candidate-3"]=0
 results["candidate-4"]=0
 results["candidate-5"]=0
 
+#Pre-simulated data is saved as a local file in order to save some time.
 results = pickle.load( open( "votes.pickle", "rb" ))
 requestPercentageMultiProcess()
 
--- server.py
+++ server.py
@@ -2,10 +2,9 @@ from flask import Flask, url_for, render_template, request, redirect, jsonify, m
 import json
 import countmeup
 from countmeup import User
+
 import pdb as debugger
 from urllib2 import HTTPError
-
-
 import pyrebase
 
 app = Flask(__name__)
@@ -16,9 +15,10 @@ config = {
 	"databaseURL": "https://count-me-up-2d6d6.firebaseio.com",
 	"storageBucket": "count-me-up-2d6d6.appspot.com",
 }
-
+#Config data for firebase
 
 firebase = pyrebase.initialize_app(config)
+#Python wrapper for firebase
 auth = firebase.auth()
 db = firebase.database()
 app.secret_key = 'so-secret'
@@ -27,7 +27,9 @@ app.secret_key = 'so-secret'
 def index():
 
 	results = countmeup.requestPercentageMultiProcess()
+	#The application is RESTful therefore we can process the output easily
 	if 'user' in session:
+		#Checking if the user is logged in, if that is the case we simply keep the user auth data in session
 		print "user logged in"
 		print session['user'].get('email')
 
@@ -40,13 +42,34 @@ def index():
 
 @app.route('/vote',methods=['POST'])
 def vote():
+	results = countmeup.requestPercentageMultiProcess()
+	user_vote_count=0
+	#I chosed to keep votes on the server side and not in the session in order to ensure the vote limit cannot be broken
 	if 'user' in session:
+		#if the user is logged in
+		email = session["user"].get("email")
+		#users 'private-key'
 		if request.form["candidate"]:
-			email = session["user"].get("email")
-			vote_data = {"email":email, "vote":request.form["candidate"]}
-			db.child("votes").push(vote_data)
-			countmeup.vote(User("someone"),request.form["candidate"])
-		return redirect(url_for('index'))
+			#just to ensure if the user selected a candidate (its always true but just in case, purely for design purposes)
+			all_votes = db.child("votes").get()
+			#now we get all the votes in the db
+			for vote in all_votes.each():
+					print(vote.key())
+					print(vote.val())
+					if(vote.val()["email"]==email):
+						#this vote is casted by current user
+						user_vote_count+=1
+					#this is the total votes casted by the user in the PAST
+			if user_vote_count < 3:
+				vote_data = {"email":email, "vote":request.form["candidate"]}
+				db.child("votes").push(vote_data)
+				#we record a new vote is casted to the database.
+				user_vote_count+=1
+				countmeup.vote(User("anon"),request.form["candidate"])
+				#but we compute the votes mutually exclusively from the database.
+				return render_template('index.html',results=results,message='Your vote is recorded. You have '+str(3-user_vote_count)+' votes left')
+			else:
+				return render_template('index.html',results=results,message='Sorry, you reached your maximum vote allowance')
 	else:
 		return render_template('login.html')
 
@@ -56,13 +79,15 @@ def vote():
 def register():
 	email = request.form["email"]
 	password = request.form["password"]
-	status=json.dumps({})
 	try:
+		#try-except is a good design choice in order to handle success/failure status of the register user-case
 		user = auth.create_user_with_email_and_password(email, password)
-		return redirect(url_for('index', status=user))
+		session['user']=user
+		return redirect(url_for('index'))
 
 	except Exception, e:
-		status = json.dumps({"status":"failed"})
+		print e
+		return render_template('login.html',message='This username is already taken :(')
 		pass
 	#user = auth.sign_in_with_email_and_password(email, password)
 
@@ -83,15 +108,18 @@ def loginsucess():
 	try:
 		user = auth.sign_in_with_email_and_password(email, password)
 		session['user']=user
+		#we keep the logged in users in sessions
 		print session
 		return redirect(url_for('index'))
 	except Exception, e:
+		#just to handle all sorts of exceptions and errors
 		return render_template('login.html',message='wrong email/password combination')
 
 
 
 @app.route('/fetch',methods=[ 'GET'])
 def fetchResultsREST():
+	#This method is for AJAX request that is executed every second. This method returns the JSON of the results.
 	return jsonify(countmeup.requestPercentageMultiProcess())
 
 if __name__ == "__main__":
